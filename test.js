a68cc2732fcd34dfcaecb522e5938a13f0a92928
rrggv274 <rrggv274@gmail.com>
2026-03-10 10:21:15 +0100

    test

---
+++ test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View>
+      <Text>Try editing me! 🎉</Text>
+    </View>
+  );
+};
+
+export default YourApp;
